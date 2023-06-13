0a25e3fdb179b93ec5f35e982b2ffa84bcf43ce1
Matheus L. Bernardi <matheus.bernardi@lnls.br>
2023-06-13 17:08:14 -0300

    first commit
    Signed-off-by: Matheus L. Bernardi <matheus.bernardi@lnls.br>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,126 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import { create } from "zustand";
+import { motion } from "framer-motion";
+import { Lightbulb, HelpCircle } from "lucide-react";
+
+type Store = {
+  selected: string[];
+  append: (word: string) => void;
+  remove: (word: string) => void;
+};
+
+const useStore = create<Store>()((set) => ({
+  selected: [],
+  append: (word: string) =>
+    set((state) => ({ selected: [...state.selected, word] })),
+  remove: (word: string) =>
+    set((state) => ({ selected: state.selected.filter((w) => w !== word) })),
+}));
+
+const words = [
+  "Banana",
+  "Fogo",
+  "Frio",
+  "Foguete",
+  "Carro",
+  "Chocolate",
+  "Computador",
+  "Janela",
+  "Livro",
+  "Palmeiras",
+  "Loja",
+  "Vaca",
+  "Vento",
+  "Copo",
+  "Caneta",
+  "Chave",
+];
+
+const answers = {
+  tempo: ["Fogo", "Frio", "Vento", "Vaca"],
+  comida: ["Banana", "Chocolate", "Palmeiras", "Copo"],
+  casa: ["Janela", "Livro", "Loja", "Chave"],
+  transporte: ["Foguete", "Carro", "Computador", "Caneta"],
+};
+
+const ToggleButton = ({
+  word,
+  children,
+}: {
+  word: string;
+  children: React.ReactNode;
+}) => {
+  const { selected, append, remove } = useStore();
+  const [isOn, setIsOn] = useState(false);
+
+  const handleToggle = (word: string) => {
+    if (!isOn) {
+      append(word);
+    } else {
+      remove(word);
+    }
+    setIsOn((isOn) => !isOn);
+  };
+
+  return (
+    <>
+      {isOn ? (
+        <button
+          className="group bg-purple-600 rounded border border-transparent lg:p-10 transition-colors hover:scale-110 hover:border-amber-400  hover:dark:bg-purple-400"
+          onClick={() => handleToggle(word)}
+        >
+          <h2 className={`text-xs lg:text-2xl font-semibold`}>{children}</h2>
+        </button>
+      ) : (
+        <button
+          disabled={selected.length > 3}
+          onClick={() => handleToggle(word)}
+          className="group bg-slate-800 rounded border border-transparent px-4 py-8 lg:p-10 transition-colors hover:animate-wiggle hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`text-xs lg:text-2xl font-semibold`}>{children}</h2>
+        </button>
+      )}
+    </>
+  );
+};
 
 export default function Home() {
+  const [bears, setBears] = useState(words);
+  const { selected } = useStore();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        <div className="absolute top-0 right-0 h-12 w-12 py-2">
+          <button className="flex rounded-full border border-neutral-700 transition-colors hover:dark:border-neutral-200 hover:dark:bg-slate-800">
+            <span className="h-7 w-7 text-neutral-700 hover:text-slate-200" >?</span>
+          </button>
         </div>
+      <div className="grid -mx-12 place-content-center text-center lg:mb-0 grid-cols-4 gap-1">
+        {words.map((word) => (
+          <ToggleButton word={word} key={word}>
+            {word}
+          </ToggleButton>
+        ))}
+      </div>
+      <div className="grid grid-cols-4 gap-2">
+        <Lightbulb fill="white" size={20} />
+        <Lightbulb fill="white" size={20} />
+        <Lightbulb fill="white" size={20} />
+        <Lightbulb size={20} />
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="grid grid-cols-2 items-center justify-center gap-2">
+        <button className="bg-slate-800 rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          Misturar
+        </button>
+        <button
+          className="bg-slate-800 rounded-lg border border-transparent px-5 py-4 transition-colors hover:scale-110 hover:border-amber-400  hover:dark:bg-purple-600"
+          disabled={selected.length < 4}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Chutar
+        </button>
       </div>
     </main>
-  )
+  );
 }
